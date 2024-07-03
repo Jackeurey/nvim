ebac66c44af928f6ff75de9b8afe7e7dc45ad41d
Jack Coleman <JackColemanCS@gmail.com>
2024-07-02 20:11:26 -0400

    added autopairs to config

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -3,7 +3,19 @@ return {
   'tpope/vim-sleuth',
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
-
+  { -- autopairs
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    -- Optional dependency
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require('nvim-autopairs').setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+  },
   { -- Autoformat
     'stevearc/conform.nvim',
     lazy = false,
