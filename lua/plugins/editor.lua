84346904a7465ba317723dcc0140ec127a033122
Jackeurey <jackcolemancs@gmail.com>
2026-02-19 10:47:14 -0500

    commit to prepare for config rewrite

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -58,6 +58,7 @@ return {
     'cbochs/grapple.nvim',
     opts = {
       scope = 'git', -- also try out "git_branch"
+      icons = true,
     },
     event = { 'BufReadPost', 'BufNewFile' },
     cmd = 'Grapple',
