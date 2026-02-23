340f2d90ab3c74fcfdc1ee4a99ad2b697dff5e72
Jackeurey <jackcolemancs@gmail.com>
2026-02-22 21:12:29 -0500

    set intial keybindings

---
+++ lua/config/lsp.lua
@@ -0,0 +1,42 @@
+local keymap = vim.keymap
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf, silent = true }
+
+    -- set keybinds
+    keymap.set("n","gR", function ()
+      vim.lsp.buf.references({includeDeclaration = true})
+    end, opts)
+
+    opts.desc = "Go to declaration"
+    keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+
+    opts.desc = "Show LSP definition"
+    keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- show lsp definition
+
+
+    opts.desc = "See available code actions"
+    keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+    opts.desc = "Smart rename"
+    keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+
+    opts.desc = "Show line diagnostics"
+    keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+    opts.desc = "Go to previous diagnostic"
+    keymap.set("n", "[d", function()
+      vim.diagnostic.jump({ count = -1, float = true })
+    end, opts) -- jump to previous diagnostic in buffer
+    --
+    opts.desc = "Go to next diagnostic"
+    keymap.set("n", "]d", function()
+      vim.diagnostic.jump({ count = 1, float = true })
+    end, opts) -- jump to next diagnostic in buffer
+
+  end,
+})
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,30 +1,30 @@
 return {
   {
-    "williamboman/mason-lspconfig.nvim",
+    'williamboman/mason-lspconfig.nvim',
     opts = {
       -- list of servers for mason to install
       ensure_installed = {
-        "lua_ls",
-        "pyright"
+        'lua_ls',
+        'pyright',
       },
     },
     dependencies = {
-      { "williamboman/mason.nvim", config = true,},
-      "neovim/nvim-lspconfig",
+      { 'williamboman/mason.nvim', config = true },
+      'neovim/nvim-lspconfig',
       { 'j-hui/fidget.nvim', opts = {} },
     },
   },
   {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
     opts = {
       ensure_installed = {
-        "stylua", -- lua formatter
-        "debugpy", -- Python Debugger
-        "autopep8", -- Python Formatter
+        'stylua', -- lua formatter
+        'debugpy', -- Python Debugger
+        'autopep8', -- Python Formatter
       },
     },
     dependencies = {
-      "williamboman/mason.nvim",
+      'williamboman/mason.nvim',
     },
   },
 }
