0df7d813aa184c9293c453cbd24c68d11fabace3
Jackeurey <jackcolemancs@gmail.com>
2026-02-22 21:10:23 -0500

    added conform.lua for auto formatting

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      notify_on_error = true,
+      format_on_save = function(bufnr)
+        local disable_lsp_fallback_filetypes = { c = true, cpp = true }
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_lsp_fallback_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        python = { 'autopep8' },
+      },
+    },
+  },
+}
