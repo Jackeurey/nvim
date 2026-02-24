cf308b679aa9ea9f3bd5bd3e08784c7de4159b92
Jackeurey <jackcolemancs@gmail.com>
2026-02-23 23:31:56 -0500

    added todo comments

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = vim.g.have_nerd_font,
+    },
+  },
+}
